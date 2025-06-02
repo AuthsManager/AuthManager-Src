48a721a2fda97a1c81c846c7e2a119c47e14aaed
nffdev <nightfrost2discord@outlook.fr>
2025-06-02 21:54:55 +0200

    feat(app): dynamics admin dashboard

--- Front/src/routes/admin/Dashboard.jsx
+++ Front/src/routes/admin/Dashboard.jsx
@@ -1,5 +1,24 @@
-import Dashboard from "../dash/Dashboard";
+import { useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { useAuth } from "@/lib/hooks/useAuth";
+import Users from "./Users";
+import Licenses from "./Licenses";
+import Apps from "./Apps";
 
 export default function AdminDashboard() {
-    return <Dashboard />;
+    const navigate = useNavigate();
+    const location = useLocation();
+    const { user } = useAuth();
+    
+    useEffect(() => {
+        if (!user.applications.length) return navigate('/admin/apps');
+    }, [user]);
+
+    return (
+        <div className="space-y-8">
+            <Users />
+            <Licenses />
+            <Apps />
+        </div>
+    );
 }
